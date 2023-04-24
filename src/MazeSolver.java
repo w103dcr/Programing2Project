877582bd3872408df3d886bcb549cd36043b96f3
Rhett Godwin <125306062+Rhgodwin@users.noreply.github.com>
2023-04-24 12:55:22 -0400

    Update MazeSolver.java

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -1,6 +1,6 @@
 //Class MazeSolver, C211 Group Project Spring 2023
 //Author: Rhett Godwin
-//************ALPHA BUILD 0.8****************
+//************ALPHA BUILD 0.7****************
 //*     NOT IN 100% WORKING STATE           *
 //      TRANSVERSE METHOD WORKS BUT         *
 //      DOESNT DISPLAY RIGHT                *
@@ -17,107 +17,34 @@
 // THROUGH IT. (WALLS ARENT DETECTING YET   *
 //.08---------------------                  *
 // WALL DETECTION ADDED TO CODE             *
+//.08.1                                     *
+// REMOVED AUTO SOLVE CODE (KEEP MANUAL CODE*
+// STREAMLINED SOME CODE                    *
 //TODO--------------------------------------*
 //BACKTRACKING DETECTION                    *
-//ADD START AND END LOCATION TO THE MAZE    *
+//FIX WIN CONDITION                         *
+//                                          *
 //*******************************************
 
 package projectmazetest;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class MazeSolver
 {
 
-//check if a given cell in the maze is valid (within bounds and a '.' character)
-    public static boolean isValidSpot(char[][] maze, int row, int column)
-    {
-        if (row >= 0 && row < maze.length && column >= 0 && column < maze.length)
-        {
-            return maze[row][column] == '.';
-
-        }
-        return false;
-    }
-
-    // Transverse the maze using recursion and mark visited cells with an X
-    // character
-    public static boolean traverseMaze(char[][] maze, int row, int column)
-    {
-        if (isValidSpot(maze, row, column))
-        {
-            if (row == maze.length - 1 && column == maze.length - 1)
-            {
-                return true;
-            }
-
-            // marks the current cell as visited
-            maze[row][column] = 'X';
-
-            // up
-            boolean returnValue = traverseMaze(maze, row - 1, column);
-
-            // right
-            if (!returnValue)
-            {
-                returnValue = traverseMaze(maze, row, column + 1);
-
-                // down
-                if (!returnValue)
-                {
-                    returnValue = traverseMaze(maze, row + 1, column);
-                }
-
-                // left
-                if (!returnValue)
-                {
-                    returnValue = traverseMaze(maze, row, column - 1);
-
-                    if (returnValue)
-                    {
-
-                        // if this path leads to the end of the maze, mark the path with dots
-                        System.out.print(row + ", " + column);
-                        maze[row][column] = '.';
-                    }
-                    return returnValue;
-                }
-
-            }
-
-        }
-        return false;
-    }
-
-    // prints the final state of the maze with the solution path marked with dots
-    public static void printSolution(char[][] maze)
-    {
-        System.out.println("Solution to maze\n----------------");
-        for (int i = 0; i < maze.length; i++)
-        {
-            for (int j = 0; j < maze[j].length - 1; j++)
-            {
-                System.out.print(maze[i][j] + " ");
-                if (maze[i][j] == '.')
-                {
-                    System.out.print(".");
-                }
-
-            }
-            System.out.println("|");
-
-        }
-        System.out.println("--------------");
-    }
-
     public static void move(char[][] maze, int row, int column)
     {
         Scanner scan = new Scanner(System.in);
-        int newRow = 0;
-        int newColumn = 0;
 
-        //while Q is not pressed ask direction
+        // Initialize set to keep track of locations to prevent backtracking
+       
+
+ 
+
+        // while Q is not pressed ask direction
         while (true)
         {
             System.out.println("Enter the direction you wish to go (U)p, (D)own, (L)eft, (R)ight");
@@ -129,25 +56,29 @@ public class MazeSolver
                 break;
 
             }
-            //up if desired location is not a 1 move to location  
-            if (input.equalsIgnoreCase("u") && maze[row][column] != 1)
+
+            // up if desired location is not a 1 move to location
+            if (input.equalsIgnoreCase("u") && maze[row][column] != 1 && maze[row][column] != '*')
             {
-                if (maze[row - 1][column] != 1 && maze[row][column] != '*')
+
+                if (maze[row - 1][column] != 1)
                 {
                     System.out.println("Moving to position");
+
                     maze[row - 1][column] = '*';
                     row--;
+
                     printArray(maze);
                 } else
                 {
-                    System.out.println("Wall Detected!!!! Please choose another direction");
+                    wallDetected();
                 }
             }
-            
-            //down if desired location is not a 1 move to location  
-            else if (input.equalsIgnoreCase("d"))
+
+            // down if desired location is not a 1 move to location
+            else if (input.equalsIgnoreCase("d") && maze[row][column] != 1)
             {
-                if (maze[row + 1][column] != 1 )
+                if (maze[row + 1][column] != 1)
                 {
                     System.out.println("Moving to position");
                     maze[row + 1][column] = '*';
@@ -155,27 +86,30 @@ public class MazeSolver
                     printArray(maze);
                 } else
                 {
-                    System.out.println("Wall Detected!!!! Please choose another direction");
+                    wallDetected();
                 }
 
-                //right if desired location is not a 1 move to location  
-            } else if (input.equalsIgnoreCase("r"))
+                // right if desired location is not a 1 move to location
+            } else if (input.equalsIgnoreCase("r") && maze[row][column] != 1)
             {
-                if (maze[row][column + 1] != 1 )
+                if (maze[row][column + 1] != 1)
                 {
                     System.out.println("Moving to position");
                     maze[row][column + 1] = '*';
                     column++;
                     printArray(maze);
+                } else if (maze[row][column] == 2)
+                {
+                    System.out.println("Congrats you have reached the end of the maze");
                 } else
                 {
-                    System.out.println("Wall Detected!!!! Please choose another direction");
+                    wallDetected();
                 }
 
-              //left if desired location is not a 1 
-            } else if (input.equalsIgnoreCase("l"))
+                // left if desired location is not a 1
+            } else if (input.equalsIgnoreCase("l") && maze[row][column] != 1)
             {
-                if (maze[row][column - 1] != 1  )
+                if (maze[row][column - 1] != 1)
                 {
                     System.out.println("Moving to position");
                     maze[row][column - 1] = '*';
@@ -183,12 +117,16 @@ public class MazeSolver
                     printArray(maze);
                 } else
                 {
-                    System.out.println("Wall Detected!!!! Please choose another direction");
+                    wallDetected();
 
                 }
+            } else if (maze[row][column] == 2)
+            {
+                System.out.println("Congrats you have reached the end of the maze");
+
             } else
             {
-                System.out.println("invalid input");
+                System.out.println("invalid input or Backtracking attempted. Choose another direction");
             }
         }
 
@@ -212,9 +150,23 @@ public class MazeSolver
                 {
                     System.out.print("X");
                 }
+                if (maze[i][j] == 2)
+                {
+                    System.out.print("E");
+                }
+                if (maze[i][j] == 4)
+                {
+                    System.out.print("S");
+                }
+
                 System.out.print(" ");
             }
             System.out.println();
         }
+    }
+
+    public static void wallDetected()
+    {
+        System.out.println("Wall Detected!!!! Please choose another direction");
     }
 }
